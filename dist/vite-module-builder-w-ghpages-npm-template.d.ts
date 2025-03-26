28f66fe2be6c78848781336cd69eac653819a69d
John F Morton <morton@jmx2.com>
2025-03-26 17:55:54 -0400

    removed old versions

--- dist/vite-module-builder-w-ghpages-npm-template.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "vite-module-builder-w-ghpages-npm-template" {
-    function init(message: string): void;
-    const _default: {
-        init: typeof init;
-    };
-    export default _default;
-}
