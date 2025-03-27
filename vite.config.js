c97ff74ad6cf0622434b3dd99313e4a01ef7fed3
John F Morton <morton@jmx2.com>
2025-03-27 11:00:11 -0400

    Allow publicDir when using local dev server

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-// This is the config file used to compile the module that will be published to NPM.
-const path = require('path')
-const { defineConfig } = require('vite')
+// vite.config.js
+import { defineConfig } from 'vite'
+import path from 'path'
 import banner from 'vite-plugin-banner'
 import pkg from './package.json'
 
-// Now in UTC time. Format time as YYYY-MM-DDTHH:mm:ss.sssZ.
 const now = new Date().toISOString()
 
-module.exports = defineConfig({
-    publicDir: false,
+export default defineConfig(({ command }) => ({
+    publicDir: command === 'serve' ? 'public' : false, // Conditional publicDir
     build: {
         lib: {
             entry: path.resolve(__dirname, 'lib/jfm-video-player.ts'),
@@ -20,7 +19,7 @@ module.exports = defineConfig({
     },
     plugins: [
         banner(
-            `/**\n * name: ${pkg.name}\n * version: v${pkg.version}\n * description: ${pkg.description}\n * author: ${pkg.author}\n * repository: ${pkg.repository.url}\n * build date: ${now} \n */`
+            `/**\n * name: ${pkg.name}\n * version: v${pkg.version}\n * description: ${pkg.description}\n * author: ${pkg.author}\n * repository: ${pkg.repository.url}\n * build date: ${now}\n */`
         ),
     ],
-})
+}))
