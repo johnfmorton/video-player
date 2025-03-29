a4dd0e522871cfe9f29285259b8a65a2c19db17c
John F Morton <morton@jmx2.com>
2025-03-29 13:07:28 -0400

    updated public directory

--- vite.demo.config.js
+++ vite.demo.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import path from 'node:path'
 
 export default defineConfig({
-    publicDir: path.resolve(__dirname, 'static'), // Define your static assets directory
+    publicDir: path.resolve(__dirname, 'public'), // Define your static assets directory
     base: 'https://johnfmorton.github.io/video-player/', // Set this to the GitHub Pages URL, i.e., https://<USERNAME>.github.io/<REPO>/.
     build: {
         outDir: '_site', // This is directory where the demo site will be built. It will be published to GitHub Pages. It is not the same as the directory where the module will be built. It also is in the .gitignore file so you will not see it in the repo.
